fb9cd4c9d19fde2adc4359bb279305ba841d24fb
krill <lpmaze@gmail.com>
2019-09-27 15:00:33 -0400

    authe-service getUserWithUsername() made case insensitive

--- src/authe/authe-service.js
+++ src/authe/authe-service.js
@@ -1,35 +1,36 @@
-const {JWT_SECRET} = require('../config')
+const { JWT_SECRET } = require('../config')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const AuthService = {
-
   getUserWithUserName(db, username) {
     // console.log('USERNAME: ', username)
+    // return db('projector_users')
+    //   .where({ username })
+    //   .first('*')
     return db('projector_users')
-      .where({ username })
+      .whereRaw(`LOWER(username) LIKE LOWER('${username}')`)
       .first('*')
   },
 
-  comparePasswords(LoginUserPass, dbUserPass){
+  comparePasswords(LoginUserPass, dbUserPass) {
     // console.log('LoginUserPass: ', LoginUserPass)
     // console.log('dbUserPass: ', dbUserPass)
     return bcrypt.compare(LoginUserPass, dbUserPass)
   },
 
-  createJwt(subject, payload){
+  createJwt(subject, payload) {
     return jwt.sign(payload, JWT_SECRET, {
       subject,
       algorithm: 'HS256'
     })
   },
 
-  verifyJwt(token){
+  verifyJwt(token) {
     return jwt.verify(token, JWT_SECRET, {
       algorithms: ['HS256']
     })
   }
-
 }
 
 module.exports = AuthService
