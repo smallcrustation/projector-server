f2cb53e5d792a19a3e81f0168bf164036dba5f28
Krill <krill@k-X13.hsd1.fl.comcast.net>
2022-02-22 17:21:21 -0500

    add ssl: true to KNEX connection and changed KNEX connection to DB using seperate ENV vars instead of URLSTRING

--- src/config.js
+++ src/config.js
@@ -3,8 +3,15 @@ module.exports = {
 
   NODE_ENV: process.env.NODE_ENV || 'development',
 
-  DATABASE_URL: process.env.NODE_ENV === 'production'?
-  process.env.DATABASE_URL : process.env.LOCAL_DB_URL,
+  // DATABASE_URL: process.env.NODE_ENV === 'production'?
+  // process.env.DATABASE_URL : process.env.LOCAL_DB_URL,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE,
+
+
 
   CLIENT_ORIGIN: process.env.NODE_ENV === 'production'?
     'https://projector.krill.now.sh':'http://localhost:3000',
--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const app = require('./app')
 const knex = require('knex')
-const { PORT, DATABASE_URL } = require('./config')
+const { PORT, DATABASE_URL, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } = require('./config')
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL
+  connection: {
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    // connectionSting: DATABASE_URL,
+    ssl: true
+  }
 })
 
 app.set('db', db)
